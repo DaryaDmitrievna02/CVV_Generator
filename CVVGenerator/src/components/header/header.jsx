f6fe7523b9179598d36151e98fb80c4a296f1136
unknown <d.kolodinskaya02@gmail.com>
2024-07-07 20:36:37 +0300

    add libs

---
+++ CVVGenerator/src/components/header/header.jsx
@@ -0,0 +1,18 @@
+import { Link } from "react-router-dom";
+
+
+function Header(){
+
+
+    return(<>
+    <div className="bg-black">
+    CVV
+    
+    <Link to="/1">AA</Link>
+
+    </div>
+
+    </>)
+}
+
+export default Header;
---
+++ CVVGenerator/src/components/themeGenerator/generatorPage.jsx
@@ -0,0 +1,11 @@
+
+
+
+function GeneratorPage(){
+
+
+
+    return(<>PAGE</>)
+}
+
+export default GeneratorPage;
